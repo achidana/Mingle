4346b6f0907758bb75259a3495df28449979fd24
dimpledhawan <dhawan.dimple21@gmail.com>
2018-04-26 03:28:47 -0400

    contribute.php

--- php/contribute.php
+++ php/contribute.php
@@ -13,13 +13,19 @@ if ($conn->connect_error) {
 }
 //echo "Connected successfully";
 
-$name = $_POST['Name'];
-$loc = $_POST['Location'];
-$email = $_POST['Email'];
-$type = $_POST['Type'];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$start = $_POST['startDate'] + " " + $_POST['startTime'];
+$end = $_POST['endDate'] + " " + $_POST['endTime'];
+$url = $_POST['url'];
+$lat = (float) $_POST['lat'];
+$lng = (float) $_POST['lng'];
 
-$sql = "INSERT INTO events_info (name,location,email,type) VALUES ('$name','$loc','$email','$type')";
-//INSERT INTO mingle.events_info (name,email,start,end,url,lat,lng) VALUES ('Pancake Night', 'shreveclub@purdue.edu', '2018-04-24 21:00:00','2018-04-24 22:00:00','www.purdue.edu',40.3,-86.7)
+$sql = "INSERT INTO mingle.events_info (name,email,start,end,url,lat,lng) VALUES ('$name', '$email', '$start','$end','$email',$lat,$lng)";
+
+echo $sql;
+//$sql = "INSERT INTO events_info (name,location,email,type) VALUES ('$name','$loc','$email','$type')";
+//$sql = "INSERT INTO mingle.events_info (name,email,start,end,url,lat,lng) VALUES ('Pancake Night', 'shreveclub@purdue.edu', '2018-04-24 21:00:00','2018-04-24 22:00:00','www.purdue.edu',40.3,-86.7)";
 
 $insert = $conn->query($sql);
 
