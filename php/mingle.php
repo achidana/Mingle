ba623282e4e18768c0225c02cd94a536bb608961
dimpledhawan <dhawan.dimple21@gmail.com>
2018-04-26 00:55:17 -0400

    saving

--- php/mingle.php
+++ php/mingle.php
@@ -13,13 +13,15 @@ if ($conn->connect_error) {
 }
 //echo "Connected successfully";
 
-$name = $_POST['Name'];
 $loc = $_POST['Location'];
-$email = $_POST['Email'];
+
 $type = $_POST['Type'];
 
-$sql = "INSERT INTO events_info (name,location,email,type) VALUES ('$name','$loc','$email','$type')";
-//INSERT INTO mingle.events_info (name,email,start,end,url,lat,lng) VALUES ('Pancake Night', 'shreveclub@purdue.edu', '2018-04-24 21:00:00','2018-04-24 22:00:00','www.purdue.edu',40.3,-86.7)
+
+
+//$sql = "INSERT INTO user_info (name,location,email,type) VALUES ('$name','$loc','$email','$type')";
+//SELECT * FROM mingle.events_info
+//WHERE lat > (40.3 - .1) AND (lat < 40.3 + .1)
 
 $insert = $conn->query($sql);
 
