164261bc739479879807373b23bda27bc503d0e7
dimpledhawan <dhawan.dimple21@gmail.com>
2018-04-27 04:51:45 -0400

    login and signup php

--- php/login.php
+++ php/login.php
@@ -1,4 +1,53 @@
 <?php
+$DB_NAME = "mingle";
+$DB_USER = "mingle";
+$DB_PASSWORD = "cs252mingle";
+$DB_HOST = "mingle.cf7aqbrc9mtd.us-east-1.rds.amazonaws.com";
+$DB_PORT = 3306;
+
+$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD,$DB_NAME,$DB_PORT);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$email = $_POST['email'];
+$pwd = $_POST['pwd'];
+
+$num = "SELECT COUNT(*) FROM mingle.user_info
+WHERE email LIKE 'test1@purdue.edu';";
+
+if($num == 1) {
+  $sql = "SELECT * FROM mingle.user_info
+  WHERE email LIKE '$email';";
+
+  $result = $conn->query($sql);
+  if($pwd == $result['password']) {
+      echo json_encode("Login successful");
+  }
+
+  $conn->close();
+}
+else {
+  echo json_encode("Login failed");
+  $conn->close();
+}
+
+  // while($row = mysqli_fetch_assoc($result))
+  //     $test[] = $row;
+
+  //echo json_encode($test);
+
+
+  // if($insert) {
+  //   // echo "inserted";
+  // 	console.log("successful");
+  // }
+  // else {
+  // 	console.log("failed");
+  // }
+
+
 
 
 ?>
---
+++ php/signup.php
@@ -0,0 +1,38 @@
+<?php
+$DB_NAME = "mingle";
+$DB_USER = "mingle";
+$DB_PASSWORD = "cs252mingle";
+$DB_HOST = "mingle.cf7aqbrc9mtd.us-east-1.rds.amazonaws.com";
+$DB_PORT = 3306;
+
+$conn = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD,$DB_NAME,$DB_PORT);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$pwd = $_POST['pwd'];
+
+$sql = "INSERT INTO mingle.user_info (name,email,password) VALUES ('$name','$email','$pwd')";
+
+$result = $conn->query($sql);
+while($row = mysqli_fetch_assoc($result))
+      $test[] = $row;
+
+echo json_encode($test);
+
+
+  if($insert) {
+    // echo "inserted";
+  	console.log("successful");
+  }
+  else {
+  	console.log("failed");
+  }
+
+  $conn->close();
+
+
+?>
